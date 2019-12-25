47b2362c658c11963ad8460d6213f4567237cde2
nguyen912 <nguyenhanguyen912@gmail.com>
2019-12-25 10:15:20 +0700

    25122019

---
+++ Bien_kieu_du_lieu_va_toan_tu/Practice/BienKieuDuLieuVaToanTu/Bai1_DiemTrungBinh/DiemTrungBinh.js
@@ -0,0 +1,8 @@
+let toan = parseInt(prompt("Nhap diem toan: "));
+let ly = parseInt(prompt("Nhap diem toan: "));
+let hoa = parseInt(prompt("Nhap diem toan: "));
+let dtb = (toan + ly + hoa) /3;
+document.write("Diem trung binh la "+dtb);
+let tong = toan + ly +hoa;
+document.write("<br/>");
+document.write("Tong diem cac mon la "+tong);
---
+++ Bien_kieu_du_lieu_va_toan_tu/Practice/BienKieuDuLieuVaToanTu/Bai3_4_ChuVi_DienTichHinhTron/DienTichHinhTron.js
@@ -0,0 +1,3 @@
+R = parseInt(prompt("Nhap ban kinh hinh tron: "));
+A = Math.PI * Math.pow(R,2);
+document.write("Dien tich cua hinh tron la "+A);
