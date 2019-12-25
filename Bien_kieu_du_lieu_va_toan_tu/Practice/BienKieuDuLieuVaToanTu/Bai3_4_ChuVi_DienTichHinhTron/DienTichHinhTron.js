957a8199b8010757f0cf73e907f7af19d65abe7a
nguyen912 <nguyenhanguyen912@gmail.com>
2019-12-25 10:20:42 +0700

    25122019

--- Bien_kieu_du_lieu_va_toan_tu/Practice/BienKieuDuLieuVaToanTu/Bai3_4_ChuVi_DienTichHinhTron/DienTichHinhTron.js
+++ Bien_kieu_du_lieu_va_toan_tu/Practice/BienKieuDuLieuVaToanTu/Bai3_4_ChuVi_DienTichHinhTron/DienTichHinhTron.js
@@ -1,3 +1,6 @@
 R = parseInt(prompt("Nhap ban kinh hinh tron: "));
 A = Math.PI * Math.pow(R,2);
 document.write("Dien tich cua hinh tron la "+A);
+C = 2 * Math.PI * R;
+document.write("<br/>");
+document.write("Chu vi cua hinh tron la "+C);
