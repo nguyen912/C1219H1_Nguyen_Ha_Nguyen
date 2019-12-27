5897c06b6653a32198d573e36101f5b979b7e383
nguyen912 <nguyenhanguyen912@gmail.com>
2019-12-27 10:49:05 +0700

    27122019

---
+++ Module1/Bien_kieu_du_lieu_va_toan_tu/Exercise/UngDungMayTinh/UngDungMayTinh.js
@@ -0,0 +1,24 @@
+function add() {
+    let n1 = document.getElementById("in1").value;
+    let n2 = document.getElementById("in2").value;
+    let result = document.getElementById("result");
+    result.innerText = (n1+n2).toString();
+}
+function sub() {
+    let n1 = document.getElementById("in1").value;
+    let n2 = document.getElementById("in2").value;
+    let result = document.getElementById("result");
+    result.innerText = (n1-n2).toString();
+}
+function mul() {
+    let n1 = document.getElementById("in1").value;
+    let n2 = document.getElementById("in2").value;
+    let result = document.getElementById("result");
+    result.innerText = (n1*n2).toString();
+}
+function div() {
+    let n1 = document.getElementById("in1").value;
+    let n2 = document.getElementById("in2").value;
+    let result = document.getElementById("result");
+    result.innerText = (n1/n2).toString();
+}
