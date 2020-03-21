e4123efe5d15e07939956d0587315f52392ca4aa
Your Name <nguyenhanguyen912@gmail.com>
2020-03-21 20:55:47 +0700

    2020

--- FuramaResort/src/commons/ScannerUtils.java
+++
@@ -1,7 +0,0 @@
-package commons;
-
-import java.util.Scanner;
-
-public class ScannerUtils {
-    public static Scanner scanner = new Scanner(System.in);
-}
--- FuramaResort/src/views/Main.java
+++
@@ -1,9 +0,0 @@
-package views;
-
-import controllers.MainController;
-
-public class Main {
-    public static void main(String[] args) {
-        MainController.processMain();
-    }
-}
