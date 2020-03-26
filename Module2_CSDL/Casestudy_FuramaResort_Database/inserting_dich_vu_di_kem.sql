a72ab0c4f28b70b1f5c00290845dbac249f58be5
Your Name <nguyenhanguyen912@gmail.com>
2020-03-26 21:55:04 +0700

    2020

---
+++ Module2_CSDL/Casestudy_FuramaResort_Database/inserting_dich_vu_di_kem.sql
@@ -0,0 +1,12 @@
+insert into dich_vu_di_kem (ten_dich_vu_di_kem, gia, don_vi) 
+values ('Massage', 200000, 'lan');
+insert into dich_vu_di_kem (ten_dich_vu_di_kem, gia, don_vi) 
+values ('Karaoke', 100000, 'gio');
+insert into dich_vu_di_kem (ten_dich_vu_di_kem, gia, don_vi) 
+values ('Food', 150000, 'suat');
+insert into dich_vu_di_kem (ten_dich_vu_di_kem, gia, don_vi) 
+values ('Drink', 50000, 'phan');
+insert into dich_vu_di_kem (ten_dich_vu_di_kem, gia, don_vi) 
+values ('Car', 120000, 'chuyen');
+
+select * from dich_vu_di_kem;
