35ecf9ab0fd825fb5ef77dec3fe2470ca680a4e2
Your Name <nguyenhanguyen912@gmail.com>
2020-03-29 19:08:04 +0700

    2020

--- Module2_CSDL/Casestudy_FuramaResort_Database/tasks.sql
+++ Module2_CSDL/Casestudy_FuramaResort_Database/tasks.sql
@@ -1,3 +1,5 @@
+use furama_resort;
+
 /*========================================================================================
 2.	Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là 
 một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 ký tự.*/
@@ -28,17 +30,18 @@ order by count(hop_dong.id_khach_hang);
 /*========================================================================================
 5.	Hiển thị IDKhachHang, HoTen, TenLoaiKhach, IDHopDong, TenDichVu, NgayLamHopDong, NgayKetThuc,
  TongTien (Với TongTien được tính theo công thức như sau: ChiPhiThue + SoLuong*Gia, với SoLuong 
- và Giá là từ bảng DichVuDiKem) cho tất cả các Khách hàng đã từng đặt phỏng. 
+ và Giá là từ bảng DichVuDiKem) cho tất cả các Khách hàng đã từng đặt phòng. 
  (Những Khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).*/
  
-select khach_hang.id_khach_hang, ho_ten, ten_loai_khach, hop_dong.id_hop_dong, ten_dich_vu, 
-ngay_lam_hop_dong, ngay_ket_thuc, (chi_phi_thue + so_luong * gia) as tong_tien
+select distinct khach_hang.id_khach_hang, ho_ten, ten_loai_khach, hop_dong.id_hop_dong, ten_dich_vu, 
+ngay_lam_hop_dong, ngay_ket_thuc, sum(chi_phi_thue + so_luong * gia) as tong_tien
 from  khach_hang 
 left join hop_dong on khach_hang.id_khach_hang = hop_dong.id_khach_hang
-left join loai_khach on khach_hang.id_loai_khach = loai_khach.id_loai_khach
+inner join loai_khach on khach_hang.id_loai_khach = loai_khach.id_loai_khach
 left join dich_vu on hop_dong.id_dich_vu = dich_vu.id_dich_vu
 left join hop_dong_chi_tiet on hop_dong.id_hop_dong = hop_dong_chi_tiet.id_hop_dong
-left join dich_vu_di_kem on hop_dong_chi_tiet.id_dich_vu_di_kem = dich_vu_di_kem.id_dich_vu_di_kem;
+left join dich_vu_di_kem on hop_dong_chi_tiet.id_dich_vu_di_kem = dich_vu_di_kem.id_dich_vu_di_kem
+group by khach_hang.id_khach_hang;
 
 /*========================================================================================
 6.	Hiển thị IDDichVu, TenDichVu, DienTich, ChiPhiThue, TenLoaiDichVu của tất cả các loại Dịch vụ 
